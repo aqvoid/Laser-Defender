1400180c657adbf09781d99a28759b3188c26ef5
aqvoidDev <mindeow35@gmail.com>
2025-10-31 21:42:57 +0200

    Added Screen Shake Effect
    * when get player hit

--- Laser Defender/Assets/Scripts/Particles.cs
+++ Laser Defender/Assets/Scripts/Particles.cs
@@ -1,9 +1,15 @@
-using System;
 using UnityEngine;
 
 public class Particles : MonoBehaviour
 {
     [SerializeField] private ParticleSystem particles;
+    private Health health;
+
+    private void Awake() => health = GetComponent<Health>();
+
+    private void OnEnable() => health.OnDamaged += PlayHitParticles;
+
+    private void OnDisable() => health.OnDamaged -= PlayHitParticles;
 
     public void PlayHitParticles()
     {
