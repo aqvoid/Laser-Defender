3482e18f952852d7036e72087c4ba396f0eb64ed
aqvoidDev <mindeow35@gmail.com>
2025-11-13 15:17:10 +0200

    Added Damage Popup Prototype
    - also refactored some code

---
+++ Laser Defender/Assets/Scripts/DamagePopupController.cs
@@ -0,0 +1,34 @@
+using TMPro;
+using UnityEngine;
+
+public class DamagePopupController : MonoBehaviour
+{
+    [SerializeField] private GameObject popup;
+    private Health entityHealth;
+    private GameObject popupParent;
+
+    private void Awake()
+    {
+        entityHealth = GetComponent<Health>();
+        popupParent = GameObject.FindGameObjectWithTag("Game Canvas");
+    }
+
+    private void OnEnable()
+    {
+        entityHealth.OnEntityDamaged += SpawnText;
+    }
+    private void OnDisable()
+    {
+        entityHealth.OnEntityDamaged -= SpawnText;
+    }
+
+    private void SpawnText(Health entityHealth, int damage)
+    {
+        GameObject prefab = Instantiate(popup, entityHealth.transform.position, Quaternion.identity, popupParent.transform);
+        prefab.GetComponent<TextMeshProUGUI>().text = entityHealth.GetComponent<DamageDealer>().GetDamage().ToString();
+        print("spawned");
+
+        Destroy(prefab, 10f);
+    }
+
+}
